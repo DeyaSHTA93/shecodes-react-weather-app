3a5a187ed43c2568e48847a20f8a0c5789e32a02
DeyaSHTA93 <93060098+DeyaSHTA93@users.noreply.github.com>
2022-01-07 13:37:41 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://sharp-yonath-39a7e2.netlify.app/" target="_blank">
-          Deya Shrestha
-        </a>{" "}
-        and is{" "}
-        <a
-          href="httfooters://github.com/DeyaSHTA93/shecodes-react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://sharp-yonath-39a7e2.netlify.app/" target="_blank">
+            Deya Shrestha
+          </a>{" "}
+          and is{" "}
+          <a
+            href="httfooters://github.com/DeyaSHTA93/shecodes-react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
