e0cf31c111ad9c98e4a1c44941d5cb934f1e66be
DeyaSHTA93 <93060098+DeyaSHTA93@users.noreply.github.com>
2022-01-07 13:20:47 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://sharp-yonath-39a7e2.netlify.app/" target="_blank">
+          Deya Shrestha
+        </a>{" "}
+        and is{" "}
+        <a
+          href="httfooters://github.com/DeyaSHTA93/shecodes-react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
