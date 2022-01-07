a3c3a88afef0954bd15cc99fe288cc97a2c0a2fc
DeyaSHTA93 <93060098+DeyaSHTA93@users.noreply.github.com>
2022-01-07 23:47:48 +0100

    Added link to Netlify

--- src/App.js
+++ src/App.js
@@ -23,6 +23,14 @@ export default function App() {
             rel="noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://festive-wozniak-fafb02.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
